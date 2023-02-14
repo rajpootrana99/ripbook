9476c7fccb69ffae7cb765b67d3f134dcf479d98
Muhammad Rana <muhammadrana523@gmail.com>
2023-02-14 12:29:28 +0500

    Pages fixes images

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -144,10 +144,10 @@
                     </a>
                 </div>
                 <div class="picture_container">
-                    <img loading="lazy" src="{{ asset('asset/images/Rectangle-392x.png')}}" class="pic1 img-fluid" alt="">
-                    <img loading="lazy" src="{{ asset('asset/images/Rectangle-372x.png')}}" class="pic2 img-fluid" alt="">
-                    <img loading="lazy" src="{{ asset('asset/images/Rectangle-362x.png')}}" class="pic3 img-fluid" alt="">
-                    <img loading="lazy" src="{{ asset('asset/images/Rectangle-382x.png')}}" class="pic4 img-fluid" alt="" srcset="">
+                    <img loading="lazy" src="{{ asset('asset/images/rectangle_382x.png')}}" class="pic4 img-fluid" alt="" srcset="">
+                    <img loading="lazy" src="{{ asset('asset/images/rectangle_392x.png')}}" class="pic1 img-fluid" alt="">
+                    <img loading="lazy" src="{{ asset('asset/images/rectangle_372x.png')}}" class="pic2 img-fluid" alt="">
+                    <img loading="lazy" src="{{ asset('asset/images/rectangle_362x.png')}}" class="pic3 img-fluid" alt="">
                 </div>
             </div>
         </div>
